b49059f967b1bbb2746651f8286c589764d0efb8
Si Beaumont <simon.beaumont@citrix.com>
2013-09-23 10:46:21 +0100

    Support one-many github-branch--local-branch relation
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- build-pull-request.sh
+++ build-pull-request.sh
@@ -19,38 +19,50 @@ else
 fi
 echo "------------------------------------------------------------------------"
 echo "Finding local branch for '${ghprbTargetBranch}' of '${GIT_URL}'..."
-local_branch=$(grep -e "\s${repo_name}\srefs/heads/${ghprbTargetBranch}\s" \
+local_branches=$(grep -e "\s${repo_name}\srefs/heads/${ghprbTargetBranch}\s" \
     ~xenhg/git-subscriptions | cut -d' ' -f5 | cut -d/ -f2)
-if [ -z $local_branch ]; then
+case $(echo local_branches | wc -w) in
+0)
     echo "Error: Local build branch not found in git-subscriptions."
     exit 1
-fi
-echo "Github branch '${ghprbTargetBranch}' -> local branch '${local_branch}'"
+    ;;
+1)
+    echo "Github branch '${ghprbTargetBranch}' -> local branch ${local_branches}"
+    ;;
+*)
+    echo "Github branch '${ghprbTargetBranch}' -> local branches ${local_branches}"
+    echo "All local branches will be built..."
+    ;;
+esac
+for local_branch in ${local_branches}; do
 echo "------------------------------------------------------------------------"
-echo "Fetching local branch from build system..."
-build_hg_path=/usr/local/builds/jenkins/${BUILD_TAG}
-mkdir -p ${build_hg_path}
-hg clone http://hg/carbon/${local_branch}/build.hg ${build_hg_path}
-echo "------------------------------------------------------------------------"
-echo "Populating build.hg/myrepos with build candidiate repo..."
-local_repo=${build_hg_path}/myrepos/${repo_name}
-git clone file://$WORKSPACE ${local_repo}
-git_exe="git --git-dir=${local_repo}/.git"
-${git_exe} remote set-url origin ${GIT_URL}
-${git_exe} status 2> /dev/null
-echo "------------------------------------------------------------------------"
-echo "Start the build..."
-make --directory=${build_hg_path} manifest-latest
-make --directory=${build_hg_path} ${build_system_component}-build
-echo "------------------------------------------------------------------------"
-echo "Pulling in the RPMs from the build system to be archived..."
-rpms_dir=${WORKSPACE}/rpms
-rm -rf ${rpms_dir}
-mkdir ${rpms_dir}
-cp ${build_hg_path}/output/api/RPMS/i686/* ${rpms_dir}
-echo "------------------------------------------------------------------------"
-echo "Deleting build.hg"
-sudo rm -rf ${build_hg_path}
+echo "Starting build for local branch '${local_branch}'"
+    echo "    ----------------------------------------------------------------"
+    echo "    Fetching local branch '${local_branch}' from build system..."
+    build_hg_path=/usr/local/builds/jenkins/${BUILD_TAG}/${local_branch}
+    mkdir -p ${build_hg_path}
+    hg clone http://hg/carbon/${local_branch}/build.hg ${build_hg_path}
+    echo "    ----------------------------------------------------------------"
+    echo "    Populating build.hg/myrepos with build candidiate repo..."
+    local_repo=${build_hg_path}/myrepos/${repo_name}
+    git clone file://$WORKSPACE ${local_repo}
+    git_exe="git --git-dir=${local_repo}/.git"
+    ${git_exe} remote set-url origin ${GIT_URL}
+    ${git_exe} status 2> /dev/null
+    echo "    ----------------------------------------------------------------"
+    echo "    Start the build..."
+    make --directory=${build_hg_path} manifest-latest
+    make --directory=${build_hg_path} ${build_system_component}-build
+    echo "    ----------------------------------------------------------------"
+    echo "    Extracting RPMs from '${local_branch}' build.hg for archive..."
+    rpms_dir=${WORKSPACE}/rpms-${local_branch}
+    rm -rf ${rpms_dir}
+    mkdir ${rpms_dir}
+    cp ${build_hg_path}/output/api/RPMS/i686/* ${rpms_dir}
+    echo "    ----------------------------------------------------------------"
+    echo "    Deleting build.hg for local branch '${local_branch}'"
+    sudo rm -rf ${build_hg_path}
+done
 echo "------------------------------------------------------------------------"
 echo "End of build script"
 echo "------------------------------------------------------------------------"
