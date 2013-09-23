c89c0f5ee5bf5e3e228e722930ebf0f36e7dbb09
Si Beaumont <simon.beaumont@citrix.com>
2013-09-23 15:26:13 +0100

    Fix formatting of logging
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- build-pull-request.sh
+++ build-pull-request.sh
@@ -37,30 +37,30 @@ esac
 for local_branch in ${local_branches}; do
 echo "------------------------------------------------------------------------"
 echo "Starting build for local branch '${local_branch}'"
-    echo "    ----------------------------------------------------------------"
-    echo "    Fetching local branch '${local_branch}' from build system..."
+    echo "--------------------------------------------------------------------"
+    echo "Fetching local branch '${local_branch}' from build system..."
     build_hg_path=/usr/local/builds/jenkins/${BUILD_TAG}/${local_branch}
     mkdir -p ${build_hg_path}
     hg clone http://hg/carbon/${local_branch}/build.hg ${build_hg_path}
-    echo "    ----------------------------------------------------------------"
-    echo "    Populating build.hg/myrepos with build candidiate repo..."
+    echo "--------------------------------------------------------------------"
+    echo "Populating build.hg/myrepos with build candidiate repo..."
     local_repo=${build_hg_path}/myrepos/${repo_name}
     git clone file://$WORKSPACE ${local_repo}
     git_exe="git --git-dir=${local_repo}/.git"
     ${git_exe} remote set-url origin ${GIT_URL}
     ${git_exe} status 2> /dev/null
-    echo "    ----------------------------------------------------------------"
-    echo "    Start the build..."
+    echo "--------------------------------------------------------------------"
+    echo "Start the build..."
     make --directory=${build_hg_path} manifest-latest
     make --directory=${build_hg_path} ${build_system_component}-build
-    echo "    ----------------------------------------------------------------"
-    echo "    Extracting RPMs from '${local_branch}' build.hg for archive..."
+    echo "--------------------------------------------------------------------"
+    echo "Extracting RPMs from '${local_branch}' build.hg for archive..."
     rpms_dir=${WORKSPACE}/rpms-${local_branch}
     rm -rf ${rpms_dir}
     mkdir ${rpms_dir}
     cp ${build_hg_path}/output/${build_system_component}/RPMS/i686/* ${rpms_dir}
-    echo "    ----------------------------------------------------------------"
-    echo "    Deleting build.hg for local branch '${local_branch}'"
+    echo "--------------------------------------------------------------------"
+    echo "Deleting build.hg for local branch '${local_branch}'"
     sudo rm -rf ${build_hg_path}
 done
 echo "------------------------------------------------------------------------"
