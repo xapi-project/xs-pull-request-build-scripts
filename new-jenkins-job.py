54ca0eedb9bdc3e541e40e32531c867a297d62b6
Si Beaumont <simon.beaumont@citrix.com>
2013-09-23 15:22:40 +0100

    Add --server option to new-jenkins-job.py
    Signed-off-by: Si Beaumont <simon.beaumont@citrix.com>

--- new-jenkins-job.py
+++ new-jenkins-job.py
@@ -12,6 +12,7 @@ TEMPLATE_CONFIG = "./config.xml.template"
 def usage():
     print ("""Usage: %s
            [-h|--help]
+           [-s|--server] <Jenkins server>[:port]
            [-n|--name] <Repository name>
            [-p|--project-url] <Github project URL>
            [-g|--git-url] <Git URL>
@@ -29,14 +30,14 @@ def new_config(template, name, project_url, git_url, component):
     return contents
 
 
-def new_jenkins_job(name, project_url, git_url, component):
+def new_jenkins_job(server, name, project_url, git_url, component):
     print "Creating new Jenkins Github Pull Request Builder job for %s" % name
     print "Creating build config...",
     config = new_config(TEMPLATE_CONFIG, name, project_url, git_url, component)
     print "Done"
     try:
-        print "Accessing Jenkins on localhost...",
-        j = jenkins.Jenkins('http://localhost:8080')
+        print "Accessing Jenkins on server '%s'" % server,
+        j = jenkins.Jenkins(server)
         print "Done"
         print "Creating new Jenkins job...",
         j.create_job(name, config)
@@ -49,19 +50,21 @@ def new_jenkins_job(name, project_url, git_url, component):
 
 def main():
     try:
-        opts, args = getopt.getopt(
-            sys.argv[1:], "hn:p:g:c:",
-            ["help", "name=", "project-url=", "git-url=", "component="])
+        opts, args = getopt.getopt(sys.argv[1:], "hs:n:p:g:c:",
+                                   ["help", "server=", "name=", "project-url=",
+                                    "git-url=", "component="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print str(err)
         usage()
         sys.exit(2)
-    name, project_url, git_url, component = None, None, None, None
+    server = name = project_url = git_url = component = None
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
             sys.exit()
+        elif o in ("-s", "--server"):
+            server = a
         elif o in ("-n", "--name"):
             name = a
         elif o in ("-p", "--project-url"):
@@ -72,10 +75,10 @@ def main():
             component = a
         else:
             assert False, "unhandled option"
-    if None in (name, project_url, git_url, component):
+    if None in (server, name, project_url, git_url, component):
         usage()
         sys.exit(2)
-    new_jenkins_job(name, project_url, git_url, component)
+    new_jenkins_job(server, name, project_url, git_url, component)
 
 
 if __name__ == "__main__":
